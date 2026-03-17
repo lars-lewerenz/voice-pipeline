846fdbe9fda94444f940ff686c1fc15e9663bf3a
Lars Lewerenz <git.lars.lewerenz@gmail.com>
2026-03-17 21:45:26 +0100

    use modern language features

--- server.py
+++ server.py
@@ -4,14 +4,13 @@ app = FastAPI()
 
 
 @app.websocket("/ws")
-async def echo_endpoint(websocket: WebSocket):
+async def echo_endpoint(websocket: WebSocket) -> None:
     await websocket.accept()
     try:
         while True:
             # yield control back to the event loop while waiting for incoming network packets
-            data = await websocket.receive_bytes()
-
-            # immediate echo return of the raw bytes
-            await websocket.send_bytes(data)
+            # then: directly echo the data
+            async for data in websocket.iter_bytes():
+                await websocket.send_bytes(data)
     except WebSocketDisconnect:
         print("Client disconnected.")
